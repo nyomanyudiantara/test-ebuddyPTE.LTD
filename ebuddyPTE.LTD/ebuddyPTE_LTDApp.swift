9381e1be1afd6dd917f5340d6d199f4498c40f33
a mutant <yudi.antara13@gmail.com>
2024-12-21 08:04:02 +0800

    Initial Commit

---
+++ ebuddyPTE.LTD/ebuddyPTE_LTDApp.swift
@@ -0,0 +1,17 @@
+//
+//  ebuddyPTE_LTDApp.swift
+//  ebuddyPTE.LTD
+//
+//  Created by a mutant on 21/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ebuddyPTE_LTDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ebuddyPTE.LTDTests/ebuddyPTE_LTDTests.swift
@@ -0,0 +1,17 @@
+//
+//  ebuddyPTE_LTDTests.swift
+//  ebuddyPTE.LTDTests
+//
+//  Created by a mutant on 21/12/24.
+//
+
+import Testing
+@testable import ebuddyPTE_LTD
+
+struct ebuddyPTE_LTDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
